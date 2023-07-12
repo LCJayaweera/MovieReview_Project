5f7462377c039334d76f5bf846cda07fee69c328
Lakshan <lcjayaweera@gmail.com>
2023-07-12 22:26:43 +0900

    Fetch item using ID

--- src/main/java/dev/lakshan/movies/MovieService.java
+++ src/main/java/dev/lakshan/movies/MovieService.java
@@ -1,10 +1,12 @@
 package dev.lakshan.movies;
 
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,6 +14,10 @@ public class MovieService {
     private  MovieRepository movieRepository;
     public List<Movie>allMovies(){
      return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 
 }
